6d5534ee1d56c0ab4c9eb8158f5c3b2ad8cf36c4
AnandhuRaj42 <93871968+AnandhuRaj42@users.noreply.github.com>
2022-05-24 15:51:34 +0530

    Add files via upload

--- CO_2/CO2.3.sql
+++ CO_2/CO2.3.sql
@@ -1,50 +1,35 @@
 #bank
- create table account(acno number,name varchar(20),amount number);
- insert into account values(&acno,'&name',&amount);
 
-create or replace procedure deposit(ac number,bal number) is 
+create table account(acno number,name varchar(20),balance number);
+insert into account values(&acno,'&name',&balance);
+
+create or replace procedure deposit(ac number,bal number) is
 begin
-update account set amount=amount+bal where acno=ac;
+update account set balance=balance+bal where acno=ac;
 end;
 /
 
-create or replace procedure withdraw(ac number,bal number) is 
+create or replace procedure withdraw(ac number,bal number) is
 begin
-update account set amount=amount-bal where acno=ac;
-end;
-/
-
-create or replace procedure display is
-cursor c is select * from account;
-begin
-for i in c
-loop
-dbms_output.put_line(i.acno||' '||i.name||' '||i.amount);
-end loop;
+update account set balance=balance-bal where acno=ac;
 end;
 /
 
 declare
 ac number;
-am number;
+bal number;
 ch number;
 begin
-ac:=&acno;
-am:=&amount;
+ac:=&ac;
+bal:=&bal;
 ch:=&ch;
 if ch=1
-	then
-	deposit(ac,am);
-elsif ch=2
-	then
-	withdraw(ac,am);
-elsif ch=3
-	then
-	display();
+  then
+  deposit(ac,bal);
+else
+	withdraw(ac,bal);
 end if;
 end;
-/ 
-
 
 #Exception
 
@@ -71,5 +56,92 @@ begin
 record(pid,price);
 end;
 /
+
+#triggers
+
+create table sample(sid number,sname varchar(20));
+insert into sample values(&sid,'&sname');
+
+
+create or replace trigger t1
+before insert or update or delete on sample for each row
+begin 
+raise_application_error(-20000,'you are not permitted to do insert/update/delete');
+end;
+/
+
+  create or replace trigger t2
+  after insert or update or delete on sample for each row
+  begin 
+  raise_application_error(-20000,'table updated');
+  end;
+  /
+
+#profossor
+
+create table prof(pnum number,pname varchar(20),salary number);
+insert into prof values(&pnum,'&pname',&salary);
+
+
+create or replace trigger minsalary
+before insert on prof for each row
+begin
+  if(:new.salary<60000)
+  then raise_application_error(-20004,'Violation of minimum professor salary');
+  end if;
+  end;
+  /
+
+create table profbacklog as select * from prof;
+
+
+create or replace trigger backlog 
+after delete on prof for each row
+begin 
+insert into profbacklog values(:old.pnum,:old.pname,:old.salary);
+end;
+/
+
+
+#student
+
+create table stud(roll number,name varchar(20),gender varchar(10));
+  insert into stud values(&rollno,'&name','&gender');
+create table scount(fcount number,mcount number);
+  insert into scount values(&fcount,&mcount)
+
+create or replace trigger gender
+  after insert on stud for each row
+  begin
+  if(:new.gender='m')
+  then
+  update scount set mcount=mcount+1;
+  elsif(:new.gender='f') 
+  then
+  update scount set fcount=fcount+1;
+  end if;
+  end;
+  /
+
+#gold
+
+create table gprice(cdate date,price int);
+insert into gprice values(sysdate,&price);
+create table pold(cdate date,price int);
+
+  create or replace trigger gold
+  after update on gprice for each row
+  begin
+  insert into pold values(:old.cdate,:old.price);
+  end;
+  /
+
+ 
+
+
+
+
+
+
 
 
